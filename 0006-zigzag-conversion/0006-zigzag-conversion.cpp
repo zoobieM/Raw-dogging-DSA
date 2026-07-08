57d3c0bd0741866bcc8689ebe057a0cb3b36fa2d
Juben Mehta <jubenmehta1@gmail.com>
2026-07-09 00:59:07 +0530

    Time: 10 ms (26.11%), Space: 14 MB (70.27%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        vector<string> rows(numRows);
+        int currentRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currentRow] += c;
+
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        string result = "";
+        for (string row : rows) {
+            result += row;
+        }
+
+        return result;
+    }
+};
