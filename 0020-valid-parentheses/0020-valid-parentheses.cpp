89e3cc028ff1b4a900050d09509d27a44e355996
Juben Mehta <jubenmehta1@gmail.com>
2026-07-17 19:56:38 +0530

    Time: 0 ms (100%), Space: 9 MB (38.2%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if (st.empty())
+                    return false;
+
+                if ((ch == ')' && st.top() != '(') ||
+                    (ch == '}' && st.top() != '{') ||
+                    (ch == ']' && st.top() != '[')) {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
